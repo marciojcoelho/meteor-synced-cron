a1af8dbe66d7afa9468319536c77e5afc62674dd
Márcio Coelho <marcio.coelho@tekzenit.com>
2015-06-09 12:00:38 +0100

    wip

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "Allows you to define and run scheduled jobs across multiple servers.",
   version: "1.2.1",
-  name: "percolate:synced-cron",
-  git: "https://github.com/percolatestudio/meteor-synced-cron.git"
+  name: "marciojcoelho:synced-cron",
+  git: "https://github.com/marciojcoelho/meteor-synced-cron.git"
 });
 
 Npm.depends({later: "1.1.6"});
@@ -15,6 +15,6 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
-  api.use(['percolate:synced-cron', 'tinytest']);
+  api.use(['marciojcoelho:synced-cron', 'tinytest']);
   api.add_files('synced-cron-tests.js', ['server']);
 });
